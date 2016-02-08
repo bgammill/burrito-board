708eb78c0a7f16b30a73da9d2e708f46653d1a08
Bobby Gammill <bobby.gammill@bio-techne.com>
2016-02-08 13:05:41 -0700

    Add initial code for interacting with DB

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var r = require('rethinkdb');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	r.connect({host: '192.168.99.100', port: 32775, db: 'biotechne'}, function(err, conn) {
+		if (err) throw err;
+		r.table('users').run(conn, function(err, cursor) {
+			if (err) throw err;
+			cursor.toArray(function(err, result) {
+				if (err) throw err;
+				res.send(result);
+			});
+		});
+	});
 });
 
 module.exports = router;
