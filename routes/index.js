422fcde04804c3d4dfae918823eaea59d2c9244b
Bobby Gammill <bobby.gammill@bio-techne.com>
2016-02-08 23:03:18 -0700

    Update some, add some images

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var r = require('rethinkdb');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Burrito Board' });
+
+	r.connect({host: '192.168.99.100', port: 32775, db: 'biotechne'}, function(err, conn) {
+		if (err) throw err;
+		r.table('requests').eqJoin('type', r.table('burritos')).zip().run(conn, function(err, cursor) {
+			if (err) throw err;
+			cursor.toArray(function(err, results) {
+				if (err) throw err;
+				res.render('index', { result: results, title: 'Burrito Board' });
+			});
+		});
+	});
+
 });
 
 module.exports = router;
